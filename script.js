dad19b399c942a71d0367262cd4bb0a38df6245b
NaimeDmts <naime.dmts@gmail.com>
2023-03-09 16:12:32 +0300

    5- Keydown event

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const word_el = document.getElementById('word');
 const popup = document.getElementById('popup-container');
 const message_el = document.getElementById('success-message');
 
-const correctLetters = ['j','a','v'];
+
+const correctLetters = [];
 const wrongLetters = [];
+const selectedWord = getRandomWord();
 
 function getRandomWord(){
     const words = ["javascript", "css", "java", "html","python"];
@@ -11,8 +13,7 @@ function getRandomWord(){
 }
 
 function displayWord(){
-    const selectedWord = getRandomWord();
-
+   
     word_el.innerHTML = `
     ${selectedWord.split('').map(letter => `
     <div class="letter"> ${correctLetters.includes(letter) ? letter : ''}</div>
@@ -24,6 +25,26 @@ function displayWord(){
         popup.style.display = 'flex';
         message_el.innerText = 'Tebrikler kazandınız.'
     }
-
 }
+
+window.addEventListener('keydown',function(e){
+    if(e.keyCode >= 65 && e.keyCode <= 90){
+        const letter = e.key;
+        if(selectedWord.includes(letter)){
+            if(!correctLetters.includes(letter)){
+                correctLetters.push(letter);
+                displayWord();
+            }else{
+                // bu harfi zaten eklediniz.
+                console.log('bu harfi zaten eklediniz.')
+            }
+        }else{
+            if(!wrongLetters.includes(letter)){
+                wrongLetters.push(letter);
+                //hatalı harfleri güncelle.
+                console.log('hatalı harfleri güncelle.')
+            }
+        }
+    }
+});
 displayWord();
